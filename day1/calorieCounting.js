061b903624aa2cc2c55701b8e4baa9dadb8b0cdd
Jason Humbert <jasonhumbert@outlook.com>
2022-12-02 10:04:48 +0000

    refactor for readability: add function to get file path

--- day1/calorieCounting.js
+++ day1/calorieCounting.js
@@ -1,12 +1,22 @@
 import { readFileSync } from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-const input = readFileSync('day1/input.txt', 'utf-8')
+const getFilePath = (filename) => {
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = path.dirname(__filename)
+  const inputFilePath = __dirname + '/' + filename
+  return inputFilePath
+}
+
+const inputFilePath = getFilePath('input.txt')
+const input = readFileSync(inputFilePath, 'utf-8')
+
 const inputArr = input.split("\n\n")
 const inputLength = inputArr.length
 const elfArr = inputArr.map(elf => elf.split("\n").map(foodItem => Number(foodItem)))
 
 let mostCaloriesCarried = 0
-
 for (let elf = 0; elf < inputLength; elf++) {
   let caloriesCarried = elfArr[elf].reduce((sum, currentValue) => sum + currentValue, 0)
   if (caloriesCarried > mostCaloriesCarried) {
