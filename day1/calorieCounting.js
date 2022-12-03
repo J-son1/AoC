5c2de975405afcda0254300de600727338a7a62c
Jason Humbert <jasonhumbert@outlook.com>
2022-12-03 16:52:02 +0000

    add function that takes the number of values to be returned

--- day1/calorieCounting.js
+++ day1/calorieCounting.js
@@ -13,8 +13,17 @@ const inputFilePath = getFilePath('input.txt')
 const input = readFileSync(inputFilePath, 'utf-8')
 
 const elfCaloriesCarriedArray = input.split("\n\n").map(elf => elf.split("\n").map(foodItem => Number(foodItem)))
-const elfCaloriedCarriedTotals = elfCaloriesCarriedArray.map(elf => elf.reduce((sum, currentValue) => sum + currentValue, 0))
-const sortedElfCaloriedCarriedTotals = elfCaloriedCarriedTotals.sort((a, b) => a - b)
-const mostCaloriesCarried = sortedElfCaloriedCarriedTotals.pop()
+const elfCaloriesCarriedTotals = elfCaloriesCarriedArray.map(elf => elf.reduce((sum, currentValue) => sum + currentValue, 0))
+const sortedElfCaloriedCarriedTotals = elfCaloriesCarriedTotals.sort((a, b) => a - b)
 
-console.log(mostCaloriesCarried)
+const getTopCaloriesCarried = (calorieTotalsArray, numberOfTopElves = 1) => {
+  let topCaloriesCarried = []
+  for (let elf = 0; elf < numberOfTopElves; elf++) {
+    topCaloriesCarried.push(calorieTotalsArray.pop())
+  }
+  return topCaloriesCarried
+}
+
+const mostCaloriesCarried = getTopCaloriesCarried(sortedElfCaloriedCarriedTotals)
+
+console.log(mostCaloriesCarried[0])
