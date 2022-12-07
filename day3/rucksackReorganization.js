cab89bb5ed5af118c6bed3e44961d32911f858bd
Jason Humbert <jasonhumbert@outlook.com>
2022-12-07 22:36:46 +0000

    add function to get common item types

--- day3/rucksackReorganization.js
+++ day3/rucksackReorganization.js
@@ -2,9 +2,31 @@ import { getInput } from '../helper.js'
 
 const input = getInput('day3/input.txt')
 
+// const testInput = 'vJrwpWtwJgWrhcsFMMfFFhFp'
+// const testInput = 'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL'
+
 const splitItem = (item) => {
   const halfValue = item.length / 2
   const compartment1 = item.slice(0, halfValue)
   const compartment2 = item.slice(-halfValue)
   return [compartment1, compartment2]
 }
+
+const getCommonItemTypes = (compartment1, compartment2) => {
+  let commonItemTypes = []
+  let compartment1Arr = [...compartment1]
+  let compartment2Arr = [...compartment2]
+  compartment1Arr.forEach((char1) => {
+    compartment2Arr.forEach((char2) => {
+      if (char2 == char1 && !commonItemTypes.includes(char2)) {
+        commonItemTypes.push(char2)
+      }
+    })
+  })
+  return commonItemTypes
+}
+
+// const splitItemResult = splitItem(testInput)
+// const commonItemTypes = getCommonItemTypes(splitItemResult[0], splitItemResult[1])
+
+// console.log(commonItemTypes)
