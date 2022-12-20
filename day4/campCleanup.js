501ac1b61a35a1b2e28a5d6e971496245f7bf5c5
Jason Humbert <jasonhumbert@outlook.com>
2022-12-20 11:11:47 +0000

    format input

--- day4/campCleanup.js
+++ day4/campCleanup.js
@@ -1,3 +1,8 @@
 import { getInput } from '../helper.js'
 
 const input = getInput('day4/input.txt')
+const input1 = input.split('\n')
+  .map(elfPair => elfPair.split(','))
+  .map(elfPair => elfPair.map(sections => sections.split('-')))
+  .map(elfPair => elfPair.map(sections => sections.map(section => Number(section))))
+
