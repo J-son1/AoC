40656c1dee1869485cd76c0d0a3d7c5d39584925
Jason Humbert <jasonhumbert@outlook.com>
2023-05-15 19:53:58 +0100

    add app.js

---
+++ 2022/app.js
@@ -0,0 +1,17 @@
+import { day1Answer } from './day1/calorieCounting.js'
+import { day2Answer } from './day2/rockPaperScissors.js'
+import { day3Answer } from './day3/rucksackReorganization.js'
+import { day4Answer } from './day4/campCleanup.js'
+import { day5Answer } from './day5/supplyStacks.js'
+import { day6Answer } from './day6/tuningTrouble.js'
+
+function app() {
+  console.log('Day 1 answer: ', day1Answer)
+  console.log('Day 2 answer: ', day2Answer)
+  console.log('Day 3 answer: ', day3Answer)
+  console.log('Day 4 answer: ', day4Answer)
+  console.log('Day 5 answer: ', day5Answer)
+  console.log('Day 6 answer: ', day6Answer)
+}
+
+app()
