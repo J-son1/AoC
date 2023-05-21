3f4c96c3f083613f7ba93e73e3d4a9d5f2eea88d
Jason Humbert <jasonhumbert@outlook.com>
2023-05-21 13:58:42 +0100

    add tests and FileSystem class

--- 2022/day7/noSpaceLeftOnDevice.js
+++ 2022/day7/noSpaceLeftOnDevice.js
@@ -1,3 +1,80 @@
 import { getInput } from '../helper.js'
 
 const input = getInput('day7/input.txt')
+
+export class FileSystem {
+  constructor() {
+    this.root = null
+  }
+
+  generate(input) {
+    const arrayOfArrays = inputToArrayOfArrays(input)
+    let parent
+    let current
+    let newFile
+    arrayOfArrays.forEach((line, i) => {
+      if (line[0] === '$') {
+        if (line[1] === 'cd') {
+          if (i === 0) {
+            this.root = new File('dir', line[2], [], null)
+            current = this.root
+          } else {
+            if (line[2] === '..') {
+              if (current !== this.root) {
+                let cd = this.changeDirectory(null, this.root, parent.name)
+                parent = cd.currentParent
+                current = cd.currentDirectory
+              } else {
+                return this.root
+              }
+            } else {
+              parent = current
+              current = current.content.find((file) => {
+                file.type === 'dir' && file.name === line[2]
+              })
+            }
+          }
+        }
+      }
+      if (i !== 0 && line[0] === 'dir') {
+        newFile = new File('dir', line[1], [])
+        current.content.push(newFile)
+      }
+      if (i !== 0 && !isNaN(line[0])) {
+        let filename = line.length > 2 ? `${line[1]}.${line[2]}` : line[1]
+        newFile = new File('file', filename, null, line[0])
+        current.content.push(newFile)
+      }
+    })
+  }
+
+  changeDirectory(currentParent, currentDirectory, directoryName) {
+    if (currentDirectory.type === 'dir') {
+      if (currentDirectory.name === directoryName) {
+        return { currentParent, currentDirectory }
+      }
+      let contentLength = currentDirectory.content.length
+      for (let i = 0; i < contentLength; i++) {
+        if (currentDirectory.type === 'dir') {
+          return this.changeDirectory(currentDirectory, currentDirectory.content[i], directoryName)
+        }
+      }
+    }
+  }
+}
+
+class File {
+  constructor(type, name, content, fileSize = 0) {
+    this.type = type
+    this.name = name
+    this.content = content
+    this.fileSize = fileSize
+  }
+}
+
+function inputToArrayOfArrays(input) {
+  return input
+    .split('\n')
+    .map(line => line.split(' '))
+    .map(line => /\d+/.test(line[0]) ? [Number(line[0]), ...line[1].split('.')] : line)
+}
--- 2022/day7/test/part1.test.js
+++ 2022/day7/test/part1.test.js
@@ -1,19 +1,57 @@
 import { getInput } from "../../helper.js";
-// import {} from "../tuningTrouble.js";
+import { FileSystem } from "../noSpaceLeftOnDevice.js";
 
 const testInput = getInput('day7/input-test.txt')
-const testInput1 = testInput.split('\n')
-  .map(line => line.split(' '))
-  .map(line => /\d+/.test(line[0]) ? [Number(line[0]), ...line.slice(1)] : line)
-
-console.log(testInput1)
 
 describe('part 1', () => {
+  let fileSystem
+  let commandHistory
   beforeEach(() => {
+    fileSystem = new FileSystem()
+    commandHistory = `${testInput}`
+    fileSystem.generate(commandHistory)
   })
 
-  describe('', () => {
-    it('', () => {
+  describe('FileSystem', () => {
+    describe('generate', () => {
+      describe('when given a command history', () => {
+        it('creates a new file system object with a root directory ', () => {
+          commandHistory = '$ cd /'
+          fileSystem = new FileSystem(commandHistory)
+          fileSystem.generate(commandHistory)
+          expect(fileSystem.root.name).toEqual('/')
+          expect(fileSystem.root.content.length).toEqual(0)
+        })
+
+        it('appends directories and files with a name and type', () => {
+          const expectedParent = fileSystem.root
+          const expectedChildren = fileSystem.root.content
+
+          expect(expectedParent.name).toBe('/')
+          expect(expectedParent.content.length).toBe(4)
+          expect(expectedChildren[0].name).toBe('a')
+          expect(expectedChildren[0].type).toBe('dir')
+          expect(expectedChildren[1].name).toBe('b.txt')
+          expect(expectedChildren[1].type).toBe('file')
+        })
+
+        it('creates nested directories with parent directories', () => {
+          const rootDirectory = fileSystem.root
+          const directory1down = rootDirectory.content[0]
+          const directory2down = directory1down.content[0]
+
+          expect(rootDirectory.name).toBe('/')
+          expect(rootDirectory.content.length).toBe(4)
+          expect(directory1down.name).toBe('a')
+          expect(directory1down.content.length).toBe(4)
+          expect(directory2down.name).toBe('e')
+        })
+
+        it('appends a file with a name and file size', () => {
+          expect(fileSystem.root.content[1].name).toBe('b.txt')
+          expect(fileSystem.root.content[1].fileSize).toBe(14848514)
+        })
+      })
     })
   })
 })
